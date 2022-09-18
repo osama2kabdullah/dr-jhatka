2fdb829b77301dbb7a2f7e4ab961ef2f63d9b94c
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-18 21:08:53 +0600

    footer added and dynamic year

--- src/Pages/Common/Footer.js
+++ src/Pages/Common/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
     return (
-        <div>
-            footer
+        <div className='text-center'>
+            <h4 className='p-4 text-xl'>Copyright &copy; {year} Dr. Jhatka by Osama Abdullah</h4>
         </div>
     );
 };
