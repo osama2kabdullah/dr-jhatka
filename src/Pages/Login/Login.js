eb35a9815d12b0404649aef1e9df51dd504e145b
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-18 19:28:49 +0600

    register and login dynamic now

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,102 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import AltLogin from "./AltLogin";
 
 const Login = () => {
+  const [position, setPosition] = useState(true);
+
   return (
     <section class="h-screen">
-      <div class="container px-6 py-12 h-full">
+      <div class="container py-12 h-full">
         <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-          <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
-            <img
-              src="https://gta5modsindia.com/wp-content/uploads/2022/01/Dr.Jhatka-mtou-patlu-mod.jpg"
-              className="w-full"
-              alt=""
-            />
-          </div>
-          <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
+          <div class=" lg:w-5/12 w-10/12 lg:ml-20">
             <form>
               {/* <!-- Email input --> */}
+              
+              {position || 
+              <>
               <div class="mb-6">
                 <input
                   type="text"
                   class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                  placeholder="Email address"
+                  placeholder="Full name"
+                  name="name"
                 />
               </div>
-
+              </>
+              }
+              
+              <div class="mb-6">
+                <input
+                  type="email"
+                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Email"
+                  name="email"
+                />
+              </div>
+              
               {/* <!-- Password input --> */}
               <div class="mb-6">
                 <input
                   type="password"
                   class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   placeholder="Password"
+                  name="password"
                 />
               </div>
+              {
+                position ||
+                <div class="mb-6">
+                <input
+                  type="password"
+                  class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Confirm Password"
+                  name="confPassword"
+                />
+              </div>
+              }
 
               <div class="flex justify-end items-center mb-6">
                 <a
                   href="#!"
                   class="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
                 >
-                  Forgot password?
+                  {position && "Forgot password?"}
                 </a>
               </div>
 
               {/* <!-- Submit button --> */}
               <button
                 type="submit"
-                class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
+                class="inline-block px-7 py-3 bg-sky-300 text-black font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-sky-400 hover:shadow-lg focus:bg-sky-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-sky-500 active:shadow-lg transition duration-150 ease-in-out w-full"
               >
-                Sign in
+                {position ? "Login" : "Register"}
               </button>
-
-              <div class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-                <p class="text-center font-semibold mx-4 mb-0">OR</p>
-              </div>
-
-              <button
-                class="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-                style={{ backgroundColor: "#3b5998" }}
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
-              >
-                {/* <!-- Facebook --> */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 320 512"
-                  class="w-3.5 h-3.5 mr-2"
+              <p className="p-2 text-center cursor-pointer">
+                {position
+                  ? "Don't Have an account?"
+                  : "Already have an account?"}{" "}
+                <span
+                  onClick={() => setPosition(!position)}
+                  className="text-sky-500 hover:underline"
                 >
-                  <path
-                    fill="currentColor"
-                    d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
-                  />
-                </svg>
-                Continue with Facebook
-              </button>
-              <button
-                class="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50"
-                data-mdb-ripple="true"
-                data-mdb-ripple-color="light"
-              >
-                {/* <!-- google --> */}
-                <svg
-                  class="mr-2 -ml-1 w-4 h-4"
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fab"
-                  data-icon="google"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 488 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                  ></path>
-                </svg>
-                Continue with Google
-              </button>
+                  {position ? "Register" : "Login"}
+                </span>{" "}
+              </p>
+              <AltLogin></AltLogin>
             </form>
           </div>
         </div>
