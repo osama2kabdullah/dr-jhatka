a7e6a92f345cd82efeb9d32fafd8638841e91bdd
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-18 17:58:56 +0600

    service card initialized

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Button from '../../Common/Button';
 
 const ServiceCard = ({service}) => {
-    const {name, picture, about} = service;
+    const {name, picture, about, balance} = service;
     
     return (
         <div className='bg-sky-300'>
             <img src={picture} alt="" />
             <div className='p-3'>
             <h3 className='text-2xl pb-3'>{name}</h3>
-            <p className='pb-4'>{about.slice(0,100)}</p>
+            <p className='font-bold font-mono'>Monthly {balance}</p>
+            <p className='py-4'>{about.slice(0,100)}</p>
             <Button>Choose this</Button>
             </div>
         </div>
